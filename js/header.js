b83d3feabb0bec9deb22592d4a135a6b6ae7b226
firefive2007 <98511911+firefive2007@users.noreply.github.com>
2025-09-16 11:06:43 +0300

    dadad
    adad

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <div class="header">
       <a href="./index.html">HOME</a>
       <a href="./rank your games.html">RANKING GAME</a>
-      <a href="find.html">FIND A GAME</a>
+      <a href="./lfg Game.html">FIND A GAME</a>
     </div>
   `;
   document.body.insertAdjacentHTML("afterbegin", headerHTML);
